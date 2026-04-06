01e24d9b1d39002eea143a12bf1a57d3c620c9c2
Ismael Guzman Lebron <144973003+IsmaelGL2@users.noreply.github.com>
2026-04-05 22:44:11 -0400

    Update navbar.php

--- libreria/navbar.php
+++ libreria/navbar.php
@@ -1,5 +1,22 @@
-<nav>
-    <a href="index.php">Libros</a>
-    <a href="autores.php">Autores</a>
-    <a href="contacto.php">Contacto</a>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
+  <div class="container">
+    <a class="navbar-brand fw-bold" href="index.php">📚 Librería</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="menuNavbar">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Libros</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="autores.php">Autores</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contacto.php">Contacto</a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </nav>
