b39ac94c87a213b339a19ae9672312571549c601
Ismael Guzman Lebron <144973003+IsmaelGL2@users.noreply.github.com>
2026-04-07 14:06:52 -0400

    Update navbar.php

--- libreria/navbar.php
+++ libreria/navbar.php
@@ -1,22 +1,39 @@
+<?php
+$current = basename($_SERVER['PHP_SELF']);
+?>
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
   <div class="container">
+    
     <a class="navbar-brand fw-bold" href="index.php">📚 Librería</a>
+
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuNavbar">
       <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="menuNavbar">
       <ul class="navbar-nav ms-auto">
+
         <li class="nav-item">
-          <a class="nav-link" href="index.php">Libros</a>
+          <a class="nav-link <?= ($current == 'index.php') ? 'active fw-bold text-primary' : '' ?>" href="index.php">
+            Inicio
+          </a>
         </li>
+
         <li class="nav-item">
-          <a class="nav-link" href="autores.php">Autores</a>
+          <a class="nav-link <?= ($current == 'autores.php') ? 'active fw-bold text-primary' : '' ?>" href="autores.php">
+            Autores
+          </a>
         </li>
+
         <li class="nav-item">
-          <a class="nav-link" href="contacto.php">Contacto</a>
+          <a class="nav-link <?= ($current == 'contacto.php') ? 'active fw-bold text-primary' : '' ?>" href="contacto.php">
+            Contacto
+          </a>
         </li>
+
       </ul>
     </div>
+
   </div>
 </nav>
