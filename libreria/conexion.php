88342e00e76bb301d892d234bcf28d1c275ffb90
Ismael Guzman Lebron <144973003+IsmaelGL2@users.noreply.github.com>
2026-04-07 14:04:39 -0400

    Update conexion.php

--- libreria/conexion.php
+++ libreria/conexion.php
@@ -1,11 +1,8 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-$host = "localhost";
-$db = "dblibreria";
-$user = "root";
-$pass = "";
+$host = "sql102.infinityfree.com";
+$db = "if0_41588469_dblibreria";
+$user = "if0_41588469";
+$pass = "7Yaa58Pqjb";
 
 try {
     $conexion = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
